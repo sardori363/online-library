cc270d8b5f6080d8b6bb21d045038ea55ac195f6
sardor363 <sardorbekodiljonov363@gmail.com>
2022-03-26 09:28:43 +0500

    collections deleted

--- src/main/java/com/sardor/unsplash/repository/CollectionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sardor.unsplash.repository;
-
-import com.sardor.unsplash.entity.Collection;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CollectionRepository extends JpaRepository<Collection,Integer> {
-}
